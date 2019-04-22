6f332bca14e71c42781472949d81e7c948e26973
siva98 <schokkapu1@sheffield.ac.uk>
2019-04-22 22:03:36 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,82 +1,104 @@
-package main
+
+package utils
 
 import (
-	"encoding/json"
 	"fmt"
-	"time"
+	"testing"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-var asset_name = "asset_name_a"
+type chainCode struct{}
 
-type scfinancechaincode struct {
+func (t *chainCode) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	return shim.Success(nil)
+
+}
+func (t *chainCode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	return shim.Success(nil)
 }
 
-func (t *scfinancechaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Printf(" init success")
-	return shim.Success([]byte("init success"))
+func TestNamespacePutState(t *testing.T) {
+	var value = []byte("value")
+
+	mock := shim.NewMockStub("hyperledger-fabric-evmcc", &chainCode{})
+	stub := NewNamespaceChainStubDecorator("prefix", mock)
+
+	mock.MockTransactionStart("tx1")
+	stub.PutState("key1", value)
+	mock.MockTransactionEnd("tx1")
+
+	valFromState, exists := mock.State["prefix:key1"]
+
+	assert.True(t, exists)
+	assert.Equal(t, value, valFromState)
 }
 
-func (t *scfinancechaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+func TestNamespaceGetState(t *testing.T) {
+	var value = []byte("value")
 
-	_, args := stub.GetFunctionAndParameters()
-	var opttype = args[0]
-	var assetname = args[1]
-	var optcontent = args[2]
+	mock := shim.NewMockStub("hyperledger-fabric-evmcc", &chainCode{})
+	stub := NewNamespaceChainStubDecorator("prefix", mock)
 
-	fmt.Printf("param is %s %s %s \n", opttype, assetname, optcontent)
+	mock.State["prefix:key1"] = value
 
-	if opttype == "putvalue" {
-		stub.PutState(assetname, []byte(optcontent))
-		return shim.Success([]byte("success put" + optcontent))
-	} else if opttype == "getlastvalue" {
-		var kv []byte
-		var err error
-		kv, err = stub.GetState(assetname)
-		if err != nil {
-			return shim.Error("find error!")
-		}
-		return shim.Success(kv)
-	} else if opttype == "gethistory" {
-		keysIter, err := stub.GetHistoryForKey(assetname)
-		if err != nil {
-			return shim.Error(fmt.Sprintf("GetHistoryForKey failed,Error state: %s", err))
-		}
-		defer keysIter.Close()
-		var keys []string
-		for keysIter.HasNext() {
-			response, iterErr := keysIter.Next()
-			if iterErr != nil {
-				return shim.Error(fmt.Sprintf("GetHistoryForKey operation failed,Error state: %s", err))
-			}
+	valFromState, err := stub.GetState("key1")
 
-			txid := response.TxId
-			txvalue := response.Value
-			txstate := response.IsDelete
-			txtimestamp := response.Timestamp
-			tm := time.Unix(txtimestamp.Seconds, 0)
-			datestr := tm.Format("2006-01-02 03:04:05 PM")
+	assert.NoError(t, err)
+	assert.Equal(t, value, valFromState)
+}
 
-			fmt.Printf(" Tx info - txid: %s value: %s if delete: %t datetime: %s\n", txid, string(txvalue), txstate, datestr)
-			keys = append(keys, txid)
-		}
+func TestNamespaceCreateAndSplitCompositeKey(t *testing.T) {
+	var srcArgs = []string{"a1", "a2"}
 
-		jsonKeys, err := json.Marshal(keys)
-		if err != nil {
-			return shim.Error(fmt.Sprintf("query operation failed,error is %s", err))
-		}
-		return shim.Success(jsonKeys)
-	} else {
-		return shim.Success([]byte("success invoke and No operation"))
+	mock := shim.NewMockStub("hyperledger-fabric-evmcc", &chainCode{})
+	stub := NewNamespaceChainStubDecorator("p", mock)
+
+	compKey, err := stub.CreateCompositeKey("key1", srcArgs)
+
+	assert.NoError(t, err)
+	assert.Equal(t, string([]byte{0, 0x70, 0x3a, 0x6b, 0x65, 0x79, 0x31, 0}), compKey[:8])
+
+	objType, args, err := stub.SplitCompositeKey(compKey)
+	assert.NoError(t, err)
+	assert.Equal(t, "key1", objType)
+	assert.Equal(t, srcArgs, args)
+}
+
+func TestNamespaceRangeIterator(t *testing.T) {
+	mock := shim.NewMockStub("hyperledger-fabric-evmcc", &chainCode{})
+	stub := NewNamespaceChainStubDecorator("prefix", mock)
+
+	mock.MockTransactionStart("tx1")
+	stub.PutState("key1", []byte{1})
+	stub.PutState("key2", []byte{2})
+	stub.PutState("key3", []byte{3})
+	mock.MockTransactionEnd("tx1")
+
+	valFromState := mock.State["prefix:key1"]
+	assert.Equal(t, []byte{1}, valFromState)
+	valFromState = mock.State["prefix:key2"]
+	assert.Equal(t, []byte{2}, valFromState)
+	valFromState = mock.State["prefix:key3"]
+	assert.Equal(t, []byte{3}, valFromState)
+
+	var index = 1
+
+	iter, err := stub.GetStateByRange("key1", "key3")
+	require.NoError(t, err)
+	defer iter.Close()
+	for iter.HasNext() {
+		kv, err := iter.Next()
+		require.NoError(t, err)
+
+		assert.Equal(t, fmt.Sprintf("key%d", index), kv.GetKey())
+		assert.Equal(t, uint8(index), kv.GetValue()[0])
+
+		index++
 	}
-}
-
-func main() {
-	err := shim.Start(new(scfinancechaincode))
-	if err != nil {
-		fmt.Printf("Error starting Simple chaincode: %s", err)
-	}
+	assert.Equal(t, index, 4)
 }
 
