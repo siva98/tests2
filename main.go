ec40426e0ac79056cfb2df65aa4928651136dc69
siva98 <schokkapu1@sheffield.ac.uk>
2019-04-22 21:57:53 +0100

    Update main.go

--- main.go
+++ main.go
@@ -6,137 +6,77 @@ import (
 	"time"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-	"github.com/hyperledger/fabric/protos/peer"
+	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
-// 定义结构体, 继承ChainCode接口
-type DairyFarm struct {
+var asset_name = "asset_name_a"
+
+type scfinancechaincode struct {
 }
 
-// 定义数据结构体
-type FarmInfo struct {
-	Id string
-	// 奶牛场名字
-	Name string
-	// 生产日期
-	Date string
-	// 质量等级
-	Quality string
-	// 牛奶产量, 单位t
-	Yield int
+func (t *scfinancechaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	fmt.Printf(" init success")
+	return shim.Success([]byte("init success"))
 }
 
-// 方法实现
-func (t *DairyFarm) Init(stub shim.ChaincodeStubInterface) peer.Response {
-	return t.init(stub)
-}
+func (t *scfinancechaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 
-func (t *DairyFarm) init(stub shim.ChaincodeStubInterface) peer.Response {
-	// 数据初始化
-	infos := []FarmInfo{
-		FarmInfo{Id: "DF-001", Name: "东郊农场", Date: "2018-12-11", Quality: "优", Yield: 5},
-		FarmInfo{Id: "DF-002", Name: "东郊农场", Date: "2018-12-12", Quality: "优", Yield: 5},
-		FarmInfo{Id: "DF-003", Name: "东郊农场", Date: "2018-12-13", Quality: "良", Yield: 5},
-		FarmInfo{Id: "DF-004", Name: "东郊农场", Date: "2018-12-14", Quality: "优", Yield: 5},
-		FarmInfo{Id: "DF-005", Name: "东郊农场", Date: "2018-12-15", Quality: "良", Yield: 5},
-		FarmInfo{Id: "DF-006", Name: "西郊农场", Date: "2018-12-11", Quality: "优", Yield: 6},
-		FarmInfo{Id: "DF-007", Name: "西郊农场", Date: "2018-12-12", Quality: "良", Yield: 6},
-		FarmInfo{Id: "DF-008", Name: "西郊农场", Date: "2018-12-13", Quality: "优", Yield: 6},
-		FarmInfo{Id: "DF-009", Name: "西郊农场", Date: "2018-12-14", Quality: "良", Yield: 6},
-		FarmInfo{Id: "DF-010", Name: "西郊农场", Date: "2018-12-15", Quality: "优", Yield: 6},
-		FarmInfo{Id: "DF-011", Name: "南郊农场", Date: "2018-12-11", Quality: "良", Yield: 8},
-		FarmInfo{Id: "DF-012", Name: "南郊农场", Date: "2018-12-12", Quality: "优", Yield: 8},
-		FarmInfo{Id: "DF-013", Name: "南郊农场", Date: "2018-12-13", Quality: "优", Yield: 8},
-		FarmInfo{Id: "DF-014", Name: "南郊农场", Date: "2018-12-14", Quality: "良", Yield: 8},
-		FarmInfo{Id: "DF-015", Name: "南郊农场", Date: "2018-12-15", Quality: "优", Yield: 8},
-		FarmInfo{Id: "DF-016", Name: "北郊农场", Date: "2018-12-11", Quality: "良", Yield: 3},
-		FarmInfo{Id: "DF-017", Name: "北郊农场", Date: "2018-12-12", Quality: "良", Yield: 3},
-		FarmInfo{Id: "DF-018", Name: "北郊农场", Date: "2018-12-13", Quality: "优", Yield: 3},
-		FarmInfo{Id: "DF-019", Name: "北郊农场", Date: "2018-12-14", Quality: "良", Yield: 3},
-		FarmInfo{Id: "DF-020", Name: "北郊农场", Date: "2018-12-15", Quality: "良", Yield: 3},
-	}
-	// 遍历, 写入账本中
-	i := 0
-	for i < len(infos) {
-		jsontext, error := json.Marshal(infos[i])
-		if error != nil {
-			return shim.Error("init error, json marshal fail...")
+	_, args := stub.GetFunctionAndParameters()
+	var opttype = args[0]
+	var assetname = args[1]
+	var optcontent = args[2]
+
+	fmt.Printf("param is %s %s %s \n", opttype, assetname, optcontent)
+
+	if opttype == "putvalue" {
+		stub.PutState(assetname, []byte(optcontent))
+		return shim.Success([]byte("success put" + optcontent))
+	} else if opttype == "getlastvalue" {
+		var kv []byte
+		var err error
+		kv, err = stub.GetState(assetname)
+		if err != nil {
+			return shim.Error("find error!")
 		}
-		// 数据写入账本中
-		stub.PutState(infos[i].Id, jsontext)
-		i++
-	}
-	return shim.Success([]byte("init ledger OK!!!"))
-}
-
-func (t *DairyFarm) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
-	funcName, args := stub.GetFunctionAndParameters()
-
-	if funcName == "setvalue" {
-		return t.setvalue(stub, args)
-	} else if funcName == "query" {
-		return t.query(stub, args)
-	} else if funcName == "gethistory" {
-		return t.gethistory(stub, args)
-	}
-	return shim.Success([]byte("invoke OK"))
-}
-func (t *DairyFarm) setvalue(stub shim.ChaincodeStubInterface, args []string) peer.Response {
-	keyID := args[0]
-	error := stub.PutState(keyID, []byte(args[1]))
-	if error != nil {
-		return shim.Error("PutState fail...")
-	}
-	return shim.Success([]byte("SetValue Sucess !!!"))
-}
-
-func (t *DairyFarm) query(stub shim.ChaincodeStubInterface, args []string) peer.Response {
-	keyID := args[0]
-	value, error := stub.GetState(keyID)
-	if error != nil {
-		return shim.Error("GetState fail...")
-	}
-	return shim.Success(value)
-}
-
-// 根据keyID查询历史记录
-func (t *DairyFarm) gethistory(stub shim.ChaincodeStubInterface, args []string) peer.Response {
-	keyiter, error := stub.GetHistoryForKey(args[0])
-	if error != nil {
-		return shim.Error("GetHistoryForKey fail...")
-	}
-	defer keyiter.Close()
-	// 通过迭代器对象遍历结果
-	var myList []string
-	for keyiter.HasNext() {
-		// 获取当前值
-		result, error := keyiter.Next()
-		if error != nil {
-			return shim.Error("keyiter.Next() fail...")
+		return shim.Success(kv)
+	} else if opttype == "gethistory" {
+		keysIter, err := stub.GetHistoryForKey(assetname)
+		if err != nil {
+			return shim.Error(fmt.Sprintf("GetHistoryForKey failed,Error state: %s", err))
 		}
-		// 获取需要的信息
-		txID := result.TxId
-		txValue := result.Value
-		txTime := result.Timestamp
-		txStatus := result.IsDelete
-		tm := time.Unix(txTime.Seconds, 0)
-		datastr := tm.Format("2006-01-02 15:04:05")
-		all := fmt.Sprintf("%s, %s, %s, %t", txID, txValue, datastr, txStatus)
-		myList = append(myList, all)
+		defer keysIter.Close()
+		var keys []string
+		for keysIter.HasNext() {
+			response, iterErr := keysIter.Next()
+			if iterErr != nil {
+				return shim.Error(fmt.Sprintf("GetHistoryForKey operation failed,Error state: %s", err))
+			}
+
+			txid := response.TxId
+			txvalue := response.Value
+			txstate := response.IsDelete
+			txtimestamp := response.Timestamp
+			tm := time.Unix(txtimestamp.Seconds, 0)
+			datestr := tm.Format("2006-01-02 03:04:05 PM")
+
+			fmt.Printf(" Tx info - txid: %s value: %s if delete: %t datetime: %s\n", txid, string(txvalue), txstate, datestr)
+			keys = append(keys, txid)
+		}
+
+		jsonKeys, err := json.Marshal(keys)
+		if err != nil {
+			return shim.Error(fmt.Sprintf("query operation failed,error is %s", err))
+		}
+		return shim.Success(jsonKeys)
+	} else {
+		return shim.Success([]byte("success invoke and No operation"))
 	}
-	// 数据格式化为json
-	jsonText, error := json.Marshal(myList)
-	if error != nil {
-		return shim.Error("json.Marshal(myList) fail...")
-	}
-	return shim.Success(jsonText)
 }
 
 func main() {
-	error := shim.Start(new(DairyFarm))
-	if error != nil {
-		println("程序启动失败...")
-		return
+	err := shim.Start(new(scfinancechaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
-	fmt.Println("程序启动成功...")
 }
+
